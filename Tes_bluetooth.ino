ff2a4985f127ebd920b9345ed41f412730037a13
Santo Wijaya <18214028@std.stei.itb.ac.id>
2017-08-01 22:02:10 +0700

    Bluetooth test to counting 7 segment
    Simple project to test bluetooth. More information i write on webbsant.wordpress.com

---
+++ Tes_bluetooth.ino
@@ -0,0 +1,86 @@
+//Program ini digunakan sebagai contoh untuk menghubungkan bluetooth Arduino dengan Android
+//Program akan menjadi sebuah counter sederhana
+
+char data = 0;
+//For Seven segment
+int a = 2;  //For displaying segment "a"
+int b = 3;  //For displaying segment "b"
+int c = 4;  //For displaying segment "c"
+int d = 5;  //For displaying segment "d"
+int e = 6;  //For displaying segment "e"
+int f = 8;  //For displaying segment "f"
+int g = 9;  //For displaying segment "g"
+int x=0;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(12,OUTPUT);
+  Serial.begin(9600);
+  pinMode(a, OUTPUT);  //A
+  pinMode(b, OUTPUT);  //B
+  pinMode(c, OUTPUT);  //C
+  pinMode(d, OUTPUT);  //D
+  pinMode(e, OUTPUT);  //E
+  pinMode(f, OUTPUT);  //F
+  pinMode(g, OUTPUT);  //G
+}
+
+void displayDigit(int digit)
+{
+ //Conditions for displaying segment a
+ if(digit!=1 && digit != 4)
+ digitalWrite(a,HIGH);
+ 
+ //Conditions for displaying segment b
+ if(digit != 5 && digit != 6)
+ digitalWrite(b,HIGH);
+ 
+ //Conditions for displaying segment c
+ if(digit !=2)
+ digitalWrite(c,HIGH);
+ 
+ //Conditions for displaying segment d
+ if(digit != 1 && digit !=4 && digit !=7)
+ digitalWrite(d,HIGH);
+ 
+ //Conditions for displaying segment e 
+ if(digit == 2 || digit ==6 || digit == 8 || digit==0)
+ digitalWrite(e,HIGH);
+ 
+ //Conditions for displaying segment f
+ if(digit != 1 && digit !=2 && digit!=3 && digit !=7)
+ digitalWrite(f,HIGH);
+ if (digit!=0 && digit!=1 && digit !=7)
+ digitalWrite(g,HIGH);
+ 
+}
+
+void turnOff()
+{
+  digitalWrite(a,LOW);
+  digitalWrite(b,LOW);
+  digitalWrite(c,LOW);
+  digitalWrite(d,LOW);
+  digitalWrite(e,LOW);
+  digitalWrite(f,LOW);
+  digitalWrite(g,LOW);
+}
+
+
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(Serial.available()>0){
+    data = Serial.read();
+    if(data =='1'){
+        x++;
+        turnOff();
+        displayDigit(x%9);
+        digitalWrite(12,HIGH);
+    } else if(data=='0'){
+        turnOff();
+        digitalWrite(12,LOW);
+      }
+      Serial.println(x%10);
+  }
+}
